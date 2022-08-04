1dcabdd820008f538b447964e84531d6ef29d64d
Aldi <aldialinj0@gmail.com>
2022-08-04 20:27:22 +0200

    first commit

---
+++ src/common/apis/movieApiKey.jsx
@@ -0,0 +1,1 @@
+export const APIKey = 'f7dc5017'
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss'
+
+function Footer() {
+  return (
+    <div className="footer">
+      <div>Movie App</div>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, quam? </div>
+    </div>
+  )
+}
+
+export default Footer
